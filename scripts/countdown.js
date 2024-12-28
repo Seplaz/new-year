f3969e63a38b251bb82bd03b89dfb789537b2363
Seplaz <57974538+Seplaz@users.noreply.github.com>
2024-12-28 16:50:01 +0800

    Add files via upload

---
+++ scripts/countdown.js
@@ -0,0 +1,33 @@
+// Функция для получения текущей даты и времени
+function getCurrentDate() {
+  return new Date();
+}
+
+// Функция для получения количества миллисекунд до Нового года
+function getMillisecondsUntilNewYear() {
+  const newYear = new Date(2024, 11, 31); // Год и дата Нового года
+  return Math.abs(newYear.getTime() - getCurrentDate().getTime());
+}
+
+// Функция для обновления таймера обратного отсчета
+function updateCountdownTimer() {
+  const millisecondsUntilNewYear = getMillisecondsUntilNewYear();
+
+  // Преобразование миллисекунд в дни, часы, минуты и секунды
+  const days = Math.floor(millisecondsUntilNewYear / 1000 / 60 / 60 / 24);
+  const hours = Math.floor((millisecondsUntilNewYear / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((millisecondsUntilNewYear / 1000 / 60) % 60);
+  const seconds = Math.floor((millisecondsUntilNewYear / 1000) % 60);
+
+  // Обновление отображения
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
+
+  // Повторное выполнение функции через определенные интервалы
+  setTimeout(updateCountdownTimer, 1000);
+}
+
+// Инициализация таймера обратного отсчета
+updateCountdownTimer();
